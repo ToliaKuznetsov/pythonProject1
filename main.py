2bba9efb30b38ffab9af818475519d3778d24532
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2020-10-21 19:33:52 +0700

    parallelogram

--- main.py
+++ main.py
@@ -1,67 +1,29 @@
-# Case-study #1
-# Developers:   Ivanov A. (20%),
-#               Petrova S. (60%),
-#               Sidorov M. (30%)
+
 import turtle
 
-
-def square(x, y, a):
+def parallelogram(x, y, a, b, c, r):
     '''
-    Function, drawing square.
-    :param x: upper left corner coordinate x
-    :param y: upper left corner coordinate y
-    :param a: side length of a square
-    :return: None
+    Function, drawing parallelogram.
+    :param x: upper  obtuse angle coordinate x
+    :param y: upper obtuse angle coordinate y
+    :param a: long side of the parallelogram
+    :param b: short side of the parallelogram
+    :param c: color
+    :param r: degree of rotation relative OX
     '''
-    turtle.up()
+    turtle.penup()
+    turtle.color(c)
     turtle.setposition(x, y)
-    turtle.down()
+    turtle.left(r)
+    turtle.pendown()
     turtle.forward(a)
-    turtle.right(90)
+    turtle.right(135)
+    turtle.forward(b)
+    turtle.right(45)
     turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(a)
-    turtle.right(90)
+    turtle.right(135)
+    turtle.forward(b)
+    turtle.right(45)
+    turtle.right(r)
 
 
-def triangle(x, y, a, ang, c):
-    '''
-    Function, drawing triangle.
-    :param x: upper left corner coordinate x
-    :param y: upper left corner coordinate y
-    :param a: side length of a triangle
-    :param ang: angle of a triangle
-    :param c: color of a triangle
-    :return: None
-    '''
-
-
-
-
-
-def parallelogram(x, y, a):
-    '''
-    Function, drawing square.
-    :param x: upper left corner coordinate x
-    :param y: upper left corner coordinate y
-    :param a: side length of a square
-    :return: None
-    '''
-
-
-def main():
-    '''
-    Main function.
-    :return: None
-    '''
-    square(-200, 200, 180)
-    square(20, 200, 180)
-    square(20, -20, 180)
-    square(-200, -20, 3456)
-    turtle.done()
-
-
-if __name__ == '__main__':
-    main()
