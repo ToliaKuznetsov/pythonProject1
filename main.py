2fb5c99e205d295c960576d0703843bc8aa1c971
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2020-10-23 01:26:01 +0700

    idecig

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 import turtle
 def triangle(x, y, a, b, ang, c):
     '''
-    :return: Function, drawing triangle.
+    Function, drawing triangle.
     :param x: upper left corner coordinate x
     :param y: upper left corner coordinate y
     :param a: short-side length of a triangle
@@ -14,21 +14,22 @@ def triangle(x, y, a, b, ang, c):
     :param c: color of a triangle
     :return: None
     '''
-turtle.up()
-turtle.setposition(x, y)
-turtle.setheading(ang)
-turtle.down()
-turtle.color(c)
-turtle.begin_fill()
-turtle.forward(a)
-turtle.right(90)
-turtle.forward(a)
-turtle.right(135)
-turtle.forward(b)
-turtle.right(135)
-turtle.end_fill()
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.setheading(ang)
+    turtle.down()
+    turtle.color(c)
+    turtle.begin_fill()
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(b)
+    turtle.right(135)
+    turtle.end_fill()
+
 def main():
-    triangle(1,2,10,20,0,"red")
+         triangle(1, 2, 10, 20, 0,"red")
 
 turtle.mainloop()
 
