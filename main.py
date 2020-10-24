55930c95cf6811f494e3780a5e4be2f9e541c75a
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2020-10-25 01:26:58 +0700

    Merge remote-tracking branch 'origin/Tolia' into Tolia
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -89,21 +89,40 @@ def parallelogram(x, y, a, b, r, c):
     turtle.end_fill()
     turtle.right(r)
 
-def rabbit():
+def hui():
     """
     Function, drawing rabbit.
     Tolian
     :return: None
     """
-    square(10, 260, 50, 0, '#FF9A5E')
+    square(400, 300, 50, 67.5, '#FF9A5E')
+    triangle(402,218,80,90,'#FFFF00')
+    triangle(318, 298, 80, 360, '#FF0000')
+    parallelogram(398, 166, 70, 50, 225, '#9CBF4E')
+    triangle(472, 148, 70, 180, '#00B2EE')
+    triangle(376, 138, 40, 270, '#912CEE')
+    triangle(452, 144, 40, 0, '#7D26CD')
 
-
+def hai():
+    """
+    Function, drawing rabbit.
+    Tolian
+    :return: None
+    """
+    square(400, -100, 50, 67.5, '#FF9A5E')
+    triangle(380, -189, 85, 90, '#FF0000')
+    parallelogram(376, -148, 58, 44, 225, '#9CBF4E')
+    triangle(406, -169, 85, 270, '#FFFF00')
+    triangle(410, -220, 60, 315, '#00B2EE')
+    triangle(406, -340, 44, 135, '#9B30FF')
+    triangle(282, -288, 44, 90, '#912CEE')
 def main():
     """
     Main function.
     :return: None
     """
-    rabbit()
+    hui()
+    hai()
     turtle.done()
 
 if __name__ == '__main__':
