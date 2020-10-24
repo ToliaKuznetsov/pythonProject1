2f4790f726e8f0834d707a3bba6638ee9a109f3b
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2020-10-24 23:26:49 +0700

    Merge remote-tracking branch 'origin/Tolia' into Tolia
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -3,14 +3,45 @@
 #               Petrova S. (60%),
 #               Sidorov M. (30%)
 import turtle
-def triangle(x, y, a, b, ang, c):
+turtle.setup(1200, 800)
+turtle.speed(20)
+
+
+def square(x, y, a, angle, color):
+    """
+    Function, drawing square.
+    :param x: upper left corner coordinate x
+    :param y: upper left corner coordinate y
+    :param a: side length of a square
+    :param angle: rotation angle of a square (counterclockwise)
+    :param color: color of a square
+    :return: None
+    """
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.setheading(angle)
+    turtle.left(angle)
+    turtle.color(color)
+    turtle.down()
+    turtle.begin_fill()
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.end_fill()
+    turtle.right(angle)
+
+
+def triangle(x, y, a, ang, c):
     """
     Function, drawing triangle.
     :param x: upper left corner coordinate x
     :param y: upper left corner coordinate y
     :param a: short-side length of a triangle
-    :param b: long-side length of a triangle
-    :param b: long-side length of a triangle
     :param ang: rotation angle of a triangle
     :param c: color of a triangle
     :return: None
@@ -25,11 +56,35 @@ def triangle(x, y, a, b, ang, c):
     turtle.right(90)
     turtle.forward(a)
     turtle.right(135)
-    turtle.forward(b)
+    turtle.forward(a*2**.5)
     turtle.right(135)
     turtle.end_fill()
-def main():
-    triangle(1, 2, 10, 20, 0, "red")
-turtle.mainloop()
-if __name__ == '__main__':
-    main()
+    turtle.right(ang)
+
+
+def parallelogram(x, y, a, b, r, c):
+    """
+    Function, drawing parallelogram.
+    :param x: upper  obtuse angle coordinate x
+    :param y: upper obtuse angle coordinate y
+    :param a: long side of the parallelogram
+    :param b: short side of the parallelogram
+    :param r: degree of rotation relative OX
+    :param c: color
+    """
+    turtle.penup()
+    turtle.color(c)
+    turtle.setposition(x, y)
+    turtle.left(r)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(b)
+    turtle.right(45)
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(b)
+    turtle.right(45)
+    turtle.end_fill()
+    turtle.right(r)
