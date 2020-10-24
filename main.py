30af4058cf71a9553a7bf6e7d34b65680616a441
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2020-10-25 00:09:58 +0700

    s

--- main.py
+++ main.py
@@ -25,3 +25,98 @@ def parallelogram(x, y, a, b, c, r):
     turtle.forward(b)
     turtle.right(45)
     turtle.right(r)
+
+def square(x, y, a, angle, color):
+    """
+    Function, drawing square.
+    :param x: upper left corner coordinate x
+    :param y: upper left corner coordinate y
+    :param a: side length of a square
+    :param angle: rotation angle of a square (counterclockwise)
+    :param color: color of a square
+    :return: None
+    """
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.left(angle)
+    turtle.color(color)
+    turtle.down()
+    turtle.begin_fill()
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.end_fill()
+    turtle.right(angle)
+
+
+def triangle(x, y, a, ang, c):
+    """
+    Function, drawing triangle.
+    :param x: upper left corner coordinate x
+    :param y: upper left corner coordinate y
+    :param a: short-side length of a triangle
+    :param ang: rotation angle of a triangle
+    :param c: color of a triangle
+    :return: None
+    """
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.setheading(ang)
+    turtle.down()
+    turtle.color(c)
+    turtle.begin_fill()
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(a * 2 ** .5)
+    turtle.right(135)
+    turtle.end_fill()
+    turtle.right(ang)
+
+
+def parallelogram(x, y, a, b, r, c):
+    """
+    Function, drawing parallelogram.
+    :param x: upper  obtuse angle coordinate x
+    :param y: upper obtuse angle coordinate y
+    :param a: long side of the parallelogram
+    :param b: short side of the parallelogram
+    :param r: degree of rotation relative OX
+    :param c: color
+    """
+    turtle.penup()
+    turtle.color(c)
+    turtle.setposition(x, y)
+    turtle.left(r)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(b)
+    turtle.right(45)
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(b)
+    turtle.right(45)
+    turtle.end_fill()
+    turtle.right(r)
+
+
+def rabbit():
+    """
+    Function, drawing rabbit.
+    :return: None
+    """
+    square(10, 260, 50, 0, '#FF9A5E')
+    parallelogram(-15, 264, 70, 50, 135, '#9CBF4E')
+    triangle(6, 255, 100, -90, '#FF552B')
+    triangle(-94, 51, 100, 90, '#5BC78C')
+    triangle(-15, 124, 75, -90, '#9CBF4E')
+    triangle(39, 49, 50, 180, '#F2A0B6')
+    triangle(10, 184, 50, -45, '#CCCACF')
